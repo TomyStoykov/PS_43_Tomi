1dd4e7c6a722faf870ecf4640553fa99b82fb885
Tomi <tomi.vd02@gmail.com>
2024-03-12 14:31:55 +0200

    Add project files.

---
+++ Welcome/Model/User.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Welcome.Others;
+
+namespace Welcome.Model
+{
+    internal class User
+    {
+        private string _names;
+        private string _password;
+        private UserRolesEnum _role;
+        private string _email;
+        private string _facultyNumber;
+
+        public string Names {
+            get { return _names; }
+            set { _names = value; }
+        }
+
+        public string Password
+        {
+            get { return Decrypt(_password); }
+            set { _password = Encrypt(value); }
+        }
+
+        private string Encrypt(string input)
+        {
+            char[] chars = input.ToCharArray();
+            for (int i = 0; i < chars.Length; i++)
+            {
+                chars[i] = (char)(chars[i] - 2);
+            }
+            return new string(chars);
+        }
+
+        private string Decrypt(string input)
+        {
+            char[] chars = input.ToCharArray();
+            for (int i = 0; i < chars.Length; i++)
+            {
+                chars[i] = (char)(chars[i] + 2);
+            }
+            return new string(chars);
+        }
+
+        public string Email
+        {
+            get { return _email; }
+            set { _email = value; }
+        }
+
+        public string FacultyNumber
+        {
+            get{ return _facultyNumber;}
+            set { _facultyNumber = value;}
+        }
+
+        public UserRolesEnum Role
+        {
+            get { return (UserRolesEnum)_role; }
+            set { _role = value; }
+        }
+    }
+}
---
+++ Welcome/View/UserView.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Welcome.Model;
+using Welcome.ViewModel;
+
+namespace Welcome.View
+{
+    internal class UserView
+    {
+        private UserViewModel _viewModel;
+
+        public UserView(UserViewModel viewModel, User user)
+        {
+            _viewModel = viewModel;
+            _viewModel.Name = user.Names;
+            _viewModel.Role= user.Role;
+            _viewModel.Email = user.Email;
+            _viewModel.FacultyNumber = user.FacultyNumber;
+        }
+        public enum VisualizationType
+        {
+            Standard,
+            WithOutRole
+        }
+        public void Display(VisualizationType visualization)
+        {
+            switch (visualization)
+            {
+                case VisualizationType.Standard:
+                    DisplayStandard();
+                    break;
+                case VisualizationType.WithOutRole:
+                    DisplayWithOutRole();
+                    break;
+                default:
+                    Console.WriteLine("Invalid visualization type.");
+                    break;
+            }
+        }
+
+        public void DisplayStandard()
+        {
+            Console.WriteLine("Welcome");
+            Console.WriteLine("User: " + _viewModel.Name);
+            Console.WriteLine("Role: " + _viewModel.Role);
+            Console.WriteLine("Email: " + _viewModel.Email);
+            Console.WriteLine("Faculty Number: " + _viewModel.FacultyNumber);
+        }
+
+        public void DisplayWithOutRole()
+        {
+            Console.WriteLine("Welcome");
+            Console.WriteLine("User: " + _viewModel.Name);
+            Console.WriteLine("Email: " + _viewModel.Email);
+            Console.WriteLine("Faculty Number: " + _viewModel.FacultyNumber);
+        }
+    }
+}
